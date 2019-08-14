f902c2ba4d6edb667b5630e8b512d015b80a8aa9
Monica <Monica@MoniaturePC>
2019-08-14 05:18:33 +0200

    Fixed stale issue for removal method

--- src/test/java/com/ee/EETest01.java
+++ src/test/java/com/ee/EETest01.java
@@ -18,11 +18,7 @@ import org.testng.annotations.AfterClass;
 
 public class EETest01 {
 	
-	//Problems:
-	//How to solve different kinds of browsers to run the test
-	//How to solve different versions of browsers,requiring different driver version
-	
-	static WebDriver driver, driver1, driver2;
+	static WebDriver driver;
 	static Select dropdown;
 	static WebElement fromDateBox, toDateBox;
 	
@@ -153,145 +149,42 @@ public class EETest01 {
 		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#form > div > div:nth-child(7) > input[type=button]"))).click();
 		Thread.sleep(3000);
 		
-//		//Booking 6
-//		driver.findElement(By.cssSelector("input[id='firstname']")).sendKeys("mw First Six");
-//		driver.findElement(By.cssSelector("input[id='lastname']")).sendKeys("mw Last Six");
-//		driver.findElement(By.cssSelector("input[id='totalprice']")).sendKeys("6000");
-//		
-//		dropdown.selectByVisibleText("true");
-//		
-//		 //Click Checkin Date and Wait until checkin calendar visible
-//		fromDateBox.clear();
-//		fromDateBox.click();
-//		fromDateBox.sendKeys("2017-08-05"); //Enter date in required format
-//		Thread.sleep(2000);
-//		
-//		toDateBox.clear();
-//		toDateBox.click();
-//		toDateBox.sendKeys("2019-11-05"); //Enter date in required format
-//		Thread.sleep(2000);
-//		
-//		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#form > div > div:nth-child(7) > input[type=button]"))).click();
-//		Thread.sleep(3000);
-//		
-//		//Booking 7
-//		driver.findElement(By.cssSelector("input[id='firstname']")).sendKeys("mw First Seven");
-//		driver.findElement(By.cssSelector("input[id='lastname']")).sendKeys("mw Last Seven");
-//		driver.findElement(By.cssSelector("input[id='totalprice']")).sendKeys("7000");
-//		
-//		dropdown.selectByVisibleText("true");
-//		
-//		 //Click Checkin Date and Wait until checkin calendar visible
-//		fromDateBox.clear();
-//		fromDateBox.click();
-//		fromDateBox.sendKeys("2020-05-05"); //Enter date in required format
-//		Thread.sleep(2000);
-//		
-//		toDateBox.clear();
-//		toDateBox.click();
-//		toDateBox.sendKeys("2019-10-05"); //Enter date in required format
-//		Thread.sleep(2000);
-//		
-//		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#form > div > div:nth-child(7) > input[type=button]"))).click();
-//		Thread.sleep(3000);
-//		
-//		//Booking 8
-//		driver.findElement(By.cssSelector("input[id='firstname']")).sendKeys("mw First Eight");
-//		driver.findElement(By.cssSelector("input[id='lastname']")).sendKeys("mw Last Eight");
-//		driver.findElement(By.cssSelector("input[id='totalprice']")).sendKeys("8000");
-//		
-//		dropdown.selectByVisibleText("true");
-//		
-//		 //Click Checkin Date and Wait until checkin calendar visible
-//		fromDateBox.clear();
-//		fromDateBox.click();
-//		fromDateBox.sendKeys("2018-12-05"); //Enter date in required format
-//		Thread.sleep(2000);
-//		
-//		toDateBox.clear();
-//		toDateBox.click();
-//		toDateBox.sendKeys("2019-11-05"); //Enter date in required format
-//		Thread.sleep(2000);
-//		
-//		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#form > div > div:nth-child(7) > input[type=button]"))).click();
-//		Thread.sleep(3000);
-//		
-//		//Booking 9
-//		driver.findElement(By.cssSelector("input[id='firstname']")).sendKeys("mw First Nine");
-//		driver.findElement(By.cssSelector("input[id='lastname']")).sendKeys("mw Last Nine");
-//		driver.findElement(By.cssSelector("input[id='totalprice']")).sendKeys("9000");
-//		
-//		dropdown.selectByVisibleText("true");
-//		
-//		 //Click Checkin Date and Wait until checkin calendar visible
-//		fromDateBox.clear();
-//		fromDateBox.click();
-//		fromDateBox.sendKeys("2019-06-10"); //Enter date in required format
-//		Thread.sleep(2000);
-//		
-//		toDateBox.clear();
-//		toDateBox.click();
-//		toDateBox.sendKeys("2020-01-05"); //Enter date in required format
-//		Thread.sleep(2000);
-//		
-//		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#form > div > div:nth-child(7) > input[type=button]"))).click();
-//		Thread.sleep(3000);
-//		
-//		//Booking 10
-//		driver.findElement(By.cssSelector("input[id='firstname']")).sendKeys("mw First Ten");
-//		driver.findElement(By.cssSelector("input[id='lastname']")).sendKeys("mw Last Ten");
-//		driver.findElement(By.cssSelector("input[id='totalprice']")).sendKeys("10000");
-//		
-//		dropdown.selectByVisibleText("false");
-//		
-//		 //Click Checkin Date and Wait until checkin calendar visible
-//		fromDateBox.clear();
-//		fromDateBox.click();
-//		fromDateBox.sendKeys("2018-02-20"); //Enter date in required format
-//		Thread.sleep(2000);
-//		
-//		toDateBox.clear();
-//		toDateBox.click();
-//		toDateBox.sendKeys("2019-05-05"); //Enter date in required format
-//		Thread.sleep(2000);
-//		
-//		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#form > div > div:nth-child(7) > input[type=button]"))).click();
-//		Thread.sleep(3000);
+		driver.quit();
 	}
 
 	
 	@Test
 	public void removeABooking() throws Exception {
-		//driver.navigate().refresh();
-		//beforeClass();
+		beforeClass();
 		WebDriverWait wait = new WebDriverWait(driver, 10);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 		WebElement bookingsElements = driver.findElement(By.id("bookings"));
 		List<WebElement> allDivElements = bookingsElements.findElements(By.tagName("div"));
 		List<String> idList = new ArrayList<String>();
-
+		
 	    for (WebElement e : allDivElements) {
+    	
 	        if(!e.getAttribute("id").isEmpty()) {
 	            String s = e.getAttribute("id");
 	            idList.add(s);
-	            System.out.println(idList.size());
-	            //  //*[@id="175918"]/div[1]/p (FirstName Xpath)
-	            // //*[@id="175918"]/div[7]/input (Delete Button Xpath)
-	            String deleteFirstNameXPath = "//*[@id=\"" + s + "\"]/div[1]/p";
-	            String deleteButtonXPath = "//*[@id=\"" + s + "\"]/div[7]/input";
-
-	            WebElement removeFirstName = driver.findElement(By.xpath(deleteFirstNameXPath));
-	            System.out.println(removeFirstName.getText());
-	            if (removeFirstName.getText().contains("mw")){
-	            	System.out.println("Delete");
-   	            	//must first wait for the call to complete
-	            	wait.until(ExpectedConditions.elementToBeClickable(By.xpath(deleteButtonXPath))); 	
-	            	driver.findElement(By.xpath(deleteButtonXPath)).click(); 
-	                }
 	        }
 	    }
-	    Thread.sleep(3000);
+	        for(String id : idList) {
+	            //  //*[@id="175918"]/div[1]/p (FirstName Xpath)
+	            // //*[@id="175918"]/div[7]/input (Delete Button Xpath)
+	        	String deleteFirstNameXPath = "//*[@id=\"" + id + "\"]/div[1]/p";
+	        	String deleteButtonXPath = "//*[@id=\"" + id + "\"]/div[7]/input";
+
+	        	WebElement removeFirstName = driver.findElement(By.xpath(deleteFirstNameXPath));
+
+	            if (removeFirstName.getText().contains("mw")){
+	            	wait.until(ExpectedConditions.elementToBeClickable(By.xpath(deleteButtonXPath))); 	            	
+	            	driver.findElement(By.xpath(deleteButtonXPath)).click(); 
+	                }
+		    }
+	
+    Thread.sleep(3000);
 	}
 	
     @AfterClass
